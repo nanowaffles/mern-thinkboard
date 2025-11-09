78d85a8eb6c7ee4083a762332629cbfdf9c58e6d
Christiano Contreras <nanowaffles@Christianos-MacBook-Pro.local>
2025-11-09 11:47:57 -0700

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js"
+import path from "path";
 
 import cors from "cors"
 
@@ -13,12 +14,18 @@ dotenv.config();
 //const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+// gives src path of the backend
+const __dirname = path.resolve();
 
 //() allows every single request from every single URL
-app.use(cors({
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
     origin: "http://localhost:5173"
 }));
-const PORT = process.env.PORT || 5001;
+}
+
+
 
 
 // middleware
@@ -36,6 +43,17 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    // serve our optimized react application
+    // serve as a static asset
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    // get request other than route above then we
+    // we'd like to serve our react application
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(5001, () => {
